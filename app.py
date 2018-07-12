076c1ef0fcc4bd474013e73fe3d7c2d8de930778
Tony Centurione <tonycenturione@protonmail.com>
2018-07-11 22:58:33 -0400

    Added add line and clear table methods to /enter_costs

--- app.py
+++ app.py
@@ -15,22 +15,40 @@ def index():
 
 @app.route("/enter_costs", methods=["GET", "POST"])
 def enter_costs():
+    #If page gets a get request, it will render the page. If no session history, creates an empty takeoff
     if request.method == "GET":
-        session["takeoff"] = []
+        if session.get("takeoff") is None:
+            session["takeoff"] = []
+            return render_template("enter_costs.html", takeoff=session["takeoff"])
+
+    #If add line button is pressed, the values are taken from the form and added to the takeoff list
+    try:
+        if request.form["add_line"] == "add_line":
+            item = request.form.get("item")
+            size = request.form.get("size")
+            unit = request.form.get("unit")
+            cost = request.form.get("cost")
+            note = request.form.get("note")
+            line = [item, size, unit, cost, note]
+            session["takeoff"].append(line)
+            return render_template("enter_costs.html", takeoff=session["takeoff"])
+    except: KeyError
+
+    #If the clear table button is pressed the takeoff table is cleared and the page is reset and rendered
+    try:
+        if request.form["clear_table"] == "clear_table":
+            session["takeoff"] = []
+            return render_template("enter_costs.html", takeoff=session["takeoff"])
+    except: KeyError
+
+    #Submit the table to the database.
+    try:
+        if request.form["submit_to_database"] == "submit_to_database":
+            #TODO
+            pass
+    except: KeyError
+
     
-    if session.get("takeoff") is None:
-        session["takeoff"] = []
-
-    if request.method == "POST":
-        item = request.form.get("item")
-        size = request.form.get("size")
-        unit = request.form.get("unit")
-        cost = request.form.get("cost")
-        note = request.form.get("note")
-        line = [item, size, unit, cost, note]
-        session["takeoff"].append(line)
-    return render_template("enter_costs.html", takeoff=session["takeoff"])
-
 @app.route("/build_project", methods=["GET"])
 def build_project():
     return render_template("build_project.html")
